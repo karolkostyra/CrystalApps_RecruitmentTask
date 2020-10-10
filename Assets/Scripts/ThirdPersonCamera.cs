12c445cb288a25a613b1514d2dbd5c036f9e1dec
Karol Kostyra <karol.m.kostyra@gmail.com>
2020-10-10 19:55:52 +0200

    Create third person camera controller

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [SerializeField] private Transform target, playerTransform;
+    [SerializeField] private float distanceToObject = 5.0f;
+    [SerializeField] private float rotationSpeed = 1.0f;
+
+    private Camera camera;
+    private Transform cameraTransform;
+
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private float mouseX, mouseY = 0.0f;
+
+    private Vector3 direction;
+    private Quaternion rotation;
+
+    private float savedX, savedY = 0.0f;
+
+    private Quaternion savedRotation;
+    private Quaternion startTargetRotation;
+    private Vector3 savedCameraPosition;
+    private bool wasClicked = false;
+
+
+    private void Start()
+    {
+        camera = Camera.main;
+        cameraTransform = camera.transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        startTargetRotation = target.rotation;
+
+        direction = new Vector3(0, 0, -distanceToObject);
+        cameraTransform.position = target.position + (rotation * direction);
+        cameraTransform.LookAt(target);
+    }
+
+    private void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        if (!wasClicked)
+        {
+            mouseY = Mathf.Clamp(mouseY, 0, 60);
+        }
+        else
+        {
+            mouseY = Mathf.Clamp(mouseY, -15, 90);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        if (Input.GetMouseButton(1))
+        {
+            wasClicked = true;
+            RotateCamera(rotation);
+        }
+        else
+        {
+            if (wasClicked)
+            {
+                target.rotation =
+                    Quaternion.Lerp(target.rotation, savedRotation, Time.deltaTime * 6f);
+                if(target.rotation == savedRotation)
+                {
+                    wasClicked = false;
+                    mouseX = savedX;
+                    mouseY = savedY;
+                }
+            }
+            else
+            {
+                SaveCurrentMouseInput();
+                savedRotation = target.rotation;
+                RotateCamera(rotation);
+                RotatePlayer();
+            }
+        }
+        /*
+        else if (Input.GetMouseButtonUp(1))
+        {
+            if (wasClicked)
+            {
+                Debug.Log("up");
+                target.rotation = startTargetRotation;
+                mouseX = mouseY = 0.0f;
+                //StartCoroutine(Waiter(4f));
+                //wasClicked = false;
+            }
+        }
+        else
+        {
+            Debug.Log("nothing");
+            RotateCamera(rotation);
+            RotatePlayer();
+        }
+        */
+
+        /*
+        else
+        {
+            Debug.Log("TEST");
+            if (wasClicked)
+            {
+                target.rotation = startTargetRotation;
+                mouseX = mouseY = 0.0f;
+
+                //target.localEulerAngles = new Vector3(0, 0, 0);
+                //cameraTransform.LookAt(target);
+                //cameraTransform.position = target.position + (rotation * direction);
+                wasClicked = false;
+            }
+            else
+            {
+                RotateCamera(rotation);
+                RotatePlayer();
+            }
+            //cameraTransform.position = target.position + (rotation * direction);
+            //cameraTransform.LookAt(target);
+        }
+        */
+    }
+
+    private void SaveCurrentMouseInput()
+    {
+        savedX = mouseX;
+        savedY = mouseY;
+    }
+
+    private void RotatePlayer()
+    {
+        playerTransform.rotation = Quaternion.Euler(0, mouseX, 0);
+    }
+
+    private void RotateCamera(Quaternion _rotation)
+    {
+        target.rotation = _rotation;
+    }
+
+    private void SaveCurrentCameraPos(Quaternion _rotation)
+    {
+        savedRotation = _rotation;
+    }
+
+    private void SaveCameraPosition(Vector3 _position, Quaternion _rotation, Vector3 _direction)
+    {
+        savedCameraPosition = _position + (_rotation * _direction);
+    }
+}
